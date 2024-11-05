2e9ee659b5bcd2f7f066ba168b3562281a74bab2
emre-akakce <emre@akakce.com>
2024-11-05 13:14:12 +0300

    remove GetCustomerResponse and add DTO

--- src/ArcTemplate.Application/Queries/GetCustomer/GetCustomerHandler.cs
+++ src/ArcTemplate.Application/Queries/GetCustomer/GetCustomerHandler.cs
@@ -1,9 +1,10 @@
+using ArcTemplate.Application.DTOs;
 using MediatR;
 using ArcTemplate.Core.Interfaces;
 
 namespace ArcTemplate.Application.UseCases.GetCustomer
 {
-    public class GetCustomerHandler : IRequestHandler<GetCustomerQuery, GetCustomerResponse>
+    public class GetCustomerHandler : IRequestHandler<GetCustomerQuery, CustomerDTO>
     {
         private readonly ICustomerRepository _customerRepository;
 
@@ -12,10 +13,10 @@ namespace ArcTemplate.Application.UseCases.GetCustomer
             _customerRepository = customerRepository;
         }
 
-        public Task<GetCustomerResponse> Handle(GetCustomerQuery query, CancellationToken cancellationToken)
+        public Task<CustomerDTO> Handle(GetCustomerQuery query, CancellationToken cancellationToken)
         {
             var customer = _customerRepository.GetCustomerById(query.Id);
-            return Task.FromResult(new GetCustomerResponse { Id = customer.Id, Name = customer.Name });
+            return Task.FromResult(new CustomerDTO { Id = customer.Id, Name = customer.Name });
         }
     }
 }
--- src/ArcTemplate.Application/Queries/GetCustomer/GetCustomerResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace ArcTemplate.Application.UseCases.GetCustomer
-{
-    public class GetCustomerResponse
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
